46daed57057349541bdd06664fbb5d03fcc332a1
Cemil Bayram <56959065+CemilBayram@users.noreply.github.com>
2026-09-05 23:33:18 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,7 +2,7 @@ console.log(
     "%c🔥 ICSAT CUSTOM.JS YENİ SÜRÜM ÇALIŞIYOR 🔥",
     "color:red;font-size:20px;font-weight:bold;"
 );
-console.log("ICSAT ASSETS — v1.1.4");
+console.log("ICSAT ASSETS — v1.1.5");
 
 /*
 ================================================================
